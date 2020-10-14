11ccfe002ee519479cf00cb85d4511a89b35a43c
GJYWang <gjywang@gmail.com>
2020-10-14 18:49:41 +0200

    get SV And Annotation Info

--- R_src/annotGeneSV.R
+++ R_src/annotGeneSV.R
@@ -1,4 +1,5 @@
 options(stringsAsFactors = F)
+library(stringr)
 args = commandArgs(TRUE)
 geneName = args[1]
 geneChr = args[2]
@@ -8,7 +9,10 @@ SVFile = args[5]
 annotationFile = args[6]
 
 getSVAndAnnotation <- function(geneName,geneChr,geneStart,geneEnd,SVFile,annotationFile) {
-  gzFile = gzfile(SVFile,'rt')  
+  cat(geneName,'\n')
+  geneChr = paste('chr',geneChr,sep='')
+  SVFileName = paste(SVFile,geneChr,'POS','tsv','gz',sep = '.')
+  gzFile = gzfile(SVFileName,'rt')  
   SVTable = read.table(file = gzFile, header = F, sep = '\t', quote = "")
   close(gzFile)
   SVTable[,2] = as.numeric(SVTable[,2])
@@ -23,13 +27,20 @@ getSVAndAnnotation <- function(geneName,geneChr,geneStart,geneEnd,SVFile,annotat
   close(gzFile)
   
   SVChrTable = SVTable[SVTable[,1]==geneChr,]
-  SVgeneTable = SVChrTable[(((SVChrTable[,2]>geneEnd)|(SVChrTable[,3]<geneStart))==0),]
+  SVgeneTable = SVChrTable[(((SVChrTable[,2]>=geneEnd)|(SVChrTable[,3]<=geneStart))==0),]
   
-  eltType = paste(unique(annotationTable[which(annotationTable[,9]==paste('gene_id',geneName,sep = '=')),3]),collapse = ';')
+  geneAnnoTab = annotationTable[which(str_detect(annotationTable[,9], geneName)==T),]
+  
+  eltType = rep('',nrow(SVgeneTable))
+  for (i in 1:length(eltType)) {
+    startTmp = SVgeneTable[i,2];endTmp = SVgeneTable[i,3]
+    geneAnnoTabForSV = geneAnnoTab[(((geneAnnoTab[,4]>=endTmp)|(geneAnnoTab[,5]<=startTmp))==0),]
+    if (nrow(geneAnnoTabForSV)>0) {eltType[i] = paste(unique(geneAnnoTabForSV[,3]),collapse = ';')}
+  }
   
   if (nrow(SVgeneTable)!=0) {
-    outputTSV = data.frame(variant_id = SVgeneTable[,4], gene_id = rep(geneName,nrow(SVgeneTable)), elt_type = rep(eltType,nrow(SVgeneTable)))
-    write.table(outputTSV, file=paste(SVFile,geneName,'tsv',sep = '.'), quote=FALSE, sep='\t', col.names = F, row.names = F)
+    outputTSV = data.frame(variant_id = SVgeneTable[,5], gene_id = rep(geneName,nrow(SVgeneTable)), elt_type = eltType)
+    write.table(outputTSV, file=paste(SVFile,geneName,'tsv',sep = '.'), quote=FALSE, sep='\t', col.names = T, row.names = F)
   }
 }
 
